9f4fadd95f8019eef7403ad3a5558568255bd335
Tanya <tanyach1997@gmail.com>
2022-12-11 00:25:40 +0530

    Create sku_model.sh

---
+++ sku_model.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/shayanalibhatti/Retail-Store-Item-Detection-using-YOLOv5.git
+git clone https://github.com/ultralytics/yolov5.git
+cd /content/yolov5/
+pip install -r requirements.txt
